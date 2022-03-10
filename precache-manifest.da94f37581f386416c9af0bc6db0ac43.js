b7fc430f712f1f79b7d11478badc4805ed39a36a
Nikris <nikris@mail.ru>
2022-03-10 17:44:10 +0300

    Updates

---
+++ precache-manifest.da94f37581f386416c9af0bc6db0ac43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cbf27ea19d0a9a3abdae329d83e967",
+    "url": "/Menu/index.html"
+  },
+  {
+    "revision": "4a175f5524d43b454a61",
+    "url": "/Menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "c3bc376523061cbef635",
+    "url": "/Menu/static/js/2.15f06c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Menu/static/js/2.15f06c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a175f5524d43b454a61",
+    "url": "/Menu/static/js/main.2c181efa.chunk.js"
+  },
+  {
+    "revision": "deca15953f4372325539",
+    "url": "/Menu/static/js/runtime-main.52917398.js"
+  }
+]);
